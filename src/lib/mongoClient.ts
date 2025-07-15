6fd801a031278ec54da07e92868ec495806d7d02
Muhammad Zubair Akhter <143400603+bsse23079@users.noreply.github.com>
2025-07-15 22:59:01 +0500

    d

--- src/lib/mongoClient.ts
+++ src/lib/mongoClient.ts
@@ -3,23 +3,18 @@ import { MongoClient } from 'mongodb';
 
 // Get MongoDB URI from environment variables
 const uri = process.env.MONGODB_URI || '';
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 // Throw error if URI is missing
 if (!uri) throw new Error('Please add your MongoDB URI to .env.local');
 
 // Use global variable to cache client connection
-interface GlobalWithMongoClientPromise {
-  _mongoClientPromise?: Promise<MongoClient>;
-}
-
-const globalWithMongo = globalThis as unknown as GlobalWithMongoClientPromise;
-
-let client: MongoClient;
-if (!globalWithMongo._mongoClientPromise) {
+if (!(global as any)._mongoClientPromise) {
   client = new MongoClient(uri);
-  globalWithMongo._mongoClientPromise = client.connect();
+  (global as any)._mongoClientPromise = client.connect();
 }
-const clientPromise = globalWithMongo._mongoClientPromise;
+clientPromise = (global as any)._mongoClientPromise;
 
 // Export the client promise for use in API routes
 export default clientPromise;
